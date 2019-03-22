d229be6effa475ba735e9a4523a841f3a20662fc
Philip Eskins <philipeskins@gmail.com>
2019-03-22 12:23:12 -0700

    start putting information on the page

--- src/js/logic.js
+++ src/js/logic.js
@@ -3,8 +3,8 @@ export class Doctor {
 getDoctor() {
   const apiKey = process.env.exports.apiKey;
   return new Promise(function(resolve, reject) {
-  let request = new XMLHttpRequest();
-  let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${apiKey}`;
+  const request = new XMLHttpRequest();
+  const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.451726, -122.828,323&skip=2&limit=10&user_key=${apiKey}`;
   request.onload = function() {
     if (this.status === 200) {
       resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -8,9 +8,28 @@ $(document).ready(function() {
   const doctors = new Doctor
 
   function findDoc() {
-    let promise = doctors.getDoctor();
+    const promise = doctors.getDoctor();
     promise.then(function(response) {
-      let body = JSON.parse(response);
+      const body = JSON.parse(response);
+      body.data.forEach(function(element) {
+        $("#results").append(`First Name: ${element.profile.first_name} ${element.profile.last_name}<br>`);
+        element.practices.forEach(function(element2){
+          const address = element2.visit_address;
+          $("#results").append(`Office Address: <br> ${address.street} ${address.street2}<br> ${address.city}, ${address.state} ${address.zip}`)
+          // console.log(element2.visit_address);
+          console.log(element2.phones);
+          if (element2.website === undefined) {
+            console.log(element2.website);
+          } else {
+            $("#results").append(`Website: ${element2.website}`);
+          }
+          if (element2.accepts_new_patients === true){
+            $("#results").append(`Accepts new patients: Yes<br>`)
+          } else {
+            $("#results").append(`Accepts new patients: No<br>`)
+          }
+        })
+      });
       console.log("it worked");
     }, function(error) {
       console.log("it didn't work");
@@ -19,7 +38,7 @@ $(document).ready(function() {
 
   $("form#docInput").submit(function(event) {
     event.preventDefault();
-    const condition = $("condition").val();
+    const condition = $("#condition").val();
     console.log(condition);
     findDoc();
   })
