c34381940ebdf67919c46f88f08b0c11e719e041
Philip Eskins <philipeskins@gmail.com>
2019-03-22 09:25:17 -0700

    provide a basic test to show the api is being called correctly, results are in console log

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,7 +1,21 @@
-export class Doctor Who {
+export class Doctor {
 
+getDoctor() {
+  const apiKey = process.env.exports.apiKey;
+  return new Promise(function(resolve, reject) {
+  let request = new XMLHttpRequest();
+  let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${apiKey}`;
+  request.onload = function() {
+    if (this.status === 200) {
+      resolve(request.response);
+    } else {
+      reject(Error(request.statusText));
+    }
+  }
+    request.open("GET", url, true);
+    request.send();
+  });
 
-const key = process.env.exports.apiKey
-
+}
 
 }
--- src/main.js
+++ src/main.js
@@ -2,3 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
 import './sass/styles.scss';
+import { Doctor } from './js/logic.js';
+
+$(document).ready(function() {
+  const doctors = new Doctor
+
+  function findDoc() {
+    let promise = doctors.getDoctor();
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      console.log("it worked");
+    }, function(error) {
+      console.log("it didn't work");
+    })
+  }
+
+  $("form#docInput").submit(function(event) {
+    event.preventDefault();
+    const condition = $("condition").val();
+    console.log(condition);
+    findDoc();
+  })
+
+})
