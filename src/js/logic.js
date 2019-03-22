c8ac3f0ae9471b68fe17fa1396ed825474cd3a58
Philip Eskins <philipeskins@gmail.com>
2019-03-22 13:24:45 -0700

    allow the user to search for condition and results are better formatted

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,10 +1,10 @@
 export class Doctor {
 
-getDoctor() {
+getDoctor(condition) {
   const apiKey = process.env.exports.apiKey;
   return new Promise(function(resolve, reject) {
   const request = new XMLHttpRequest();
-  const url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.451726, -122.828,323&skip=2&limit=10&user_key=${apiKey}`;
+  const url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${condition}&location=45.451726, -122.828,323&limit=10&user_key=${apiKey}`;
   request.onload = function() {
     if (this.status === 200) {
       resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -7,32 +7,40 @@ import { Doctor } from './js/logic.js';
 $(document).ready(function() {
   const doctors = new Doctor
 
-  function findDoc() {
-    const promise = doctors.getDoctor();
+  function findDoc(condition) {
+    const promise = doctors.getDoctor(condition);
     promise.then(function(response) {
       const body = JSON.parse(response);
       body.data.forEach(function(element) {
         $("#results").append(`First Name: ${element.profile.first_name} ${element.profile.last_name}<br>`);
         element.practices.forEach(function(element2){
           const address = element2.visit_address;
-          $("#results").append(`Office Address: <br> ${address.street} ${address.street2}<br> ${address.city}, ${address.state} ${address.zip}`)
-          // console.log(element2.visit_address);
-          console.log(element2.phones);
+          if(address.street2 === undefined) {
+            $("#results").append(`Office Address: <br> ${address.street}<br> ${address.city}, ${address.state} ${address.zip}<br>`)
+          } else {
+            $("#results").append(`Office Address: <br> ${address.street} ${address.street2}<br> ${address.city}, ${address.state} ${address.zip}<br>`)
+          }
+          element2.phones.forEach(function(element3){
+            // console.log(element3.number);
+            $("#results").append(`Phone Number: ${element3.number}<br>Type: ${element3.type}<br>`)
+          })
+          // console.log(element2.phones);
           if (element2.website === undefined) {
             console.log(element2.website);
           } else {
-            $("#results").append(`Website: ${element2.website}`);
+            $("#results").append(`Website: <a href=${element2.website} target="_blank">${element2.website}<br>`);
           }
           if (element2.accepts_new_patients === true){
-            $("#results").append(`Accepts new patients: Yes<br>`)
+            $("#results").append(`Accepts new patients: Yes<br><br>`)
           } else {
-            $("#results").append(`Accepts new patients: No<br>`)
+            $("#results").append(`Accepts new patients: No<br><br>`)
           }
         })
       });
-      console.log("it worked");
+      // console.log("it worked");
     }, function(error) {
-      console.log("it didn't work");
+      $('#results').text(`There was an error processing your request: ${error.message}`);
+      // console.log("it didn't work");
     })
   }
 
@@ -40,7 +48,7 @@ $(document).ready(function() {
     event.preventDefault();
     const condition = $("#condition").val();
     console.log(condition);
-    findDoc();
+    findDoc(condition);
   })
 
 })
